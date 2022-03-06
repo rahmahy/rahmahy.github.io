05ba6722c26be25dd4911f0c6e55152ec3473143
rahmah <94826010+rahmahy@users.noreply.github.com>
2022-03-06 22:35:37 +0700

    absensi

---
+++ script.js
@@ -0,0 +1,9 @@
+function closebutton(){
+    document.getElementById("navi").style.marginLeft="-150px"
+    document.getElementById("app").style.marginLeft="0"
+}
+function openbutton(){
+    document.getElementById("navi").style.width="150px"
+    document.getElementById("navi").style.marginLeft="0"
+    document.getElementById("app").style.marginLeft="150px"
+}
